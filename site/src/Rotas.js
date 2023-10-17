f34e4cbf312fa48500384f5b209aa25dad776133
samukalima20 <samuberdelima@gmail.com>
2023-10-17 17:46:01 -0300

    alterações termos e condições

--- site/src/Rotas.js
+++ site/src/Rotas.js
@@ -18,6 +18,7 @@ import DadosPedidos from        './pages/principal/dadosPedido';
 
 import QuemSomos from           './pages/rodape/quemSomos';
 import Faleconosco from         './pages/rodape/faleConosco';
+import Condicoes from           './pages/rodape/termos/index';
 
 
 
@@ -48,6 +49,7 @@ export default function Rotas(){
                 
                 <Route path='/ajuda' element={<Faleconosco />} />
                 <Route path='/quemsomos' element={<QuemSomos />}/>
+                <Route path='/termos' element={<Condicoes/>}/>
                 
              </Routes>
         </BrowserRouter>
