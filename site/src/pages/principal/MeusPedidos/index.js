a7cd72f53cec98b82af7041841c5379899666163
RitaVieiraSPP <ritacassiamelo83@gmail.com>
2023-10-21 18:10:24 -0300

    atualizzei pagina de meus pedidos

--- site/src/pages/principal/MeusPedidos/index.js
+++ site/src/pages/principal/MeusPedidos/index.js
@@ -16,17 +16,21 @@ export default function MeusPedidos(){
                <img className='tenis-travis' src='./assets/images/dunk-travis1.png' alt='travis'/>
                
                <button>Comprar novamente</button>
+
+               <hr/>
             </div>
 
             <span className='preco'>R$14.500,00</span>
-               <hr/>
+               
             <div className='tenis-2'>
                 <p>Tênis Air Jordan 12 Retro</p>
                 <img src='./assets/images/jordan12.png' alt='jordan'/>
                 <span>R$ 1.899,99</span>
                 <button>Comprar novamente</button>
+            
+               
+            
             </div>
-                <hr/>
 
             <div className='tenis-3'>
                 <p>Tênis Ultra 4DFWD</p>
