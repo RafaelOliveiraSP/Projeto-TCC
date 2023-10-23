2dfd204fe54e7a58703976f7e3bf27a4c193d080
RitaVieiraSPP <ritacassiamelo83@gmail.com>
2023-10-23 15:32:48 -0300

    atualizei pagina de meus pedidos

--- site/src/pages/principal/MeusPedidos/index.js
+++ site/src/pages/principal/MeusPedidos/index.js
@@ -9,36 +9,47 @@ export default function MeusPedidos(){
         <div className='pagina-MeusPedidos'>
             <Cabecalho/>
 
+            <div className='cartao'>
+
             <h1> Meus Pedidos </h1>
 
             <div className='tenis-1'>
-                <p>Travis Scott x Nike Dunk Low Cactus Jack Special Box</p>
                <img className='tenis-travis' src='./assets/images/dunk-travis1.png' alt='travis'/>
+               <p>Travis Scott x Nike Dunk Low Cactus <br/>Jack Special Box</p>
                
-               <button>Comprar novamente</button>
-
-               <hr/>
-            </div>
-
-            <span className='preco'>R$14.500,00</span>
-               
-            <div className='tenis-2'>
-                <p>Tênis Air Jordan 12 Retro</p>
-                <img src='./assets/images/jordan12.png' alt='jordan'/>
-                <span>R$ 1.899,99</span>
+               <div>
+                <span className='preco'>R$14.500,00</span>
                 <button>Comprar novamente</button>
-            
-               
-            
+               </div>
+                 
             </div>
+                <hr/>
+                          
+            <div className='tenis-2'>
+                <img src='./assets/images/jordan12.png' alt='jordan'/>
+                <p>Tênis Air Jordan 12 <br/>Retro</p>
+
+                <div>
+                    <span>R$ 1.899,99</span>
+                    <button>Comprar novamente</button>
+                </div>
+            </div>
+                <hr/>
+
 
             <div className='tenis-3'>
+
+                <img src='./assets/images/jordan-azul.png' alt='adidas'/>
                 <p>Tênis Ultra 4DFWD</p>
-                <img src='./assets/images/tenis-ultra.png' alt='ultra'/>
-                <span>R$ 1.999,99</span>
-                <button>Comprar novamente</button>
+
+                <div>
+                    <span>R$ 1.999,99</span>
+                    <button>Comprar novamente</button>
+                </div>
             </div>
             <Rodape/>
+
+            </div>
         </div>
     )
 }
