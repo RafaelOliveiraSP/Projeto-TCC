ab88147517a6223da66a71dd945e4b9e0f7ca743
RafaelOliveiraSP <ro0231754@gmail.com>
2023-10-31 17:49:25 -0300

    ook

--- site/src/pages/principal/minhaConta/index.js
+++ site/src/pages/principal/minhaConta/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import Cabecalho from '../../../components/cabecalho'
 import Rodape from '../../../components/rodape'
-import { Link } from 'react-router-dom'
+
 
 export default function MinhaConta(){
 
