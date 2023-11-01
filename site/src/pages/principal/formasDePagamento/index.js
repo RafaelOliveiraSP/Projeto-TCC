12a019ff868906c389d601ccdb0cd15c5035d6dd
RafaelOliveiraSP <ro0231754@gmail.com>
2023-11-01 18:26:44 -0300

    Terminei css da página forma de pagamentos

--- site/src/pages/principal/formasDePagamento/index.js
+++ site/src/pages/principal/formasDePagamento/index.js
@@ -2,9 +2,41 @@ import './index.scss';
 
 import Cabecalho from '../../../components/cabecalho';
 import Rodape from '../../../components/rodape';
+import { useState } from 'react';
 
 
 export default function FormasDePagamento(){
+
+    const [mostrarFormCred, setMostrarFormCred] = useState('none');
+    const [mostrarFormDeb, setMostrarFormDeb]   = useState('none');
+
+    const [nmrParcelas, setNmrParcelas]         = useState(0);
+
+    function mostrarTabelaCred(){
+        if(mostrarFormDeb === 'flex'){
+            setMostrarFormCred('none')
+        }
+        else if(mostrarFormCred === 'none'){
+            setMostrarFormCred('flex')  
+        }
+        else{
+            setMostrarFormCred('none')
+        }
+    }
+
+    function mostrarTabelaDeb(){
+        if(mostrarFormCred === 'flex'){
+            setMostrarFormDeb('none')
+        }
+        else if(mostrarFormDeb === 'none'){
+            setMostrarFormDeb('flex')
+        }
+        else{
+            setMostrarFormDeb('none')
+        }
+    }
+
+
 
     return(
         <div className='pagina-pagamentos'>
@@ -47,23 +79,78 @@ export default function FormasDePagamento(){
 
                         <div className='formas-de-pagamentos'>
                             <div>
-                                <i class="fa-solid fa-caret-up fa-rotate-180"></i>
+                                <i onClick={mostrarTabelaCred} class="fa-solid fa-caret-up fa-rotate-180"></i>
                                 <i class="fa-solid fa-credit-card cartao-credito"></i>
                                 <span>Cartão de crédito</span>
                             </div>
-                            <span>em 2x sem juros ou 6x no cartão</span>
+                            <span>Parcela em até 6x</span>
                         </div>
+
+                            <div className='form-cartao' style={{display: mostrarFormCred}}>
+
+                                <div>
+                                    <label>Nome impresso no cartão*</label>
+                                    <input type='text' />
+
+                                    <label>Número do cartão*</label>
+                                    <input type='text' />
+                                    <div>
+                                        <span>
+                                           <label>Vencimento*</label>
+                                            <input type='text' /> 
+                                        </span>
+                                        
+                                        <span>
+                                            <label>CVV*</label>
+                                            <input type='text' />  
+                                        </span>
+                                    </div>
+
+                                    <label id='quant-parcelas'>Parcelas*</label>
+                                    <select value={nmrParcelas} onChange={e => setNmrParcelas(e.target.value)}>
+                                        <option>Selecione</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                    </select>
+                                </div>
+                            </div>
 
                         <hr />
 
                         <div className='formas-de-pagamentos'>
                             <div>
-                                <i class="fa-solid fa-caret-up fa-rotate-180"></i>
+                                <i onClick={mostrarTabelaDeb} class="fa-solid fa-caret-up fa-rotate-180"></i>
                                 <i class="fa-solid fa-credit-card cartao-credito" style={{color: '#F7D736',}}></i>
                                 <span>Cartão de débito</span>
                             </div>
                             <span>à vista</span>
                         </div>
+
+                            <div className='form-cartao' style={{display: mostrarFormDeb}}>
+
+                                <div>
+                                    <label>Nome impresso no cartão*</label>
+                                    <input type='text' />
+
+                                    <label>Número do cartão*</label>
+                                    <input type='text' />
+                                    <div>
+                                        <span>
+                                           <label>Vencimento*</label>
+                                            <input type='text' /> 
+                                        </span>
+                                        
+                                        <span>
+                                            <label>CVV*</label>
+                                            <input type='text' />  
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
 
                         <hr />
 
